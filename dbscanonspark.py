2d79ac67009736b49bdb224ce57e6f4c117761a5
cheungshirley <57556151+cheungshirley@users.noreply.github.com>
2021-11-11 16:04:49 +0800

    Update dbscanonspark.py

--- dbscanonspark.py
+++ dbscanonspark.py
@@ -1,8 +1,4 @@
-from pyspark.sql import SparkSession
-  
-spark = SparkSession.builder.getOrCreate()
-  
-df = spark.read.format("text").load("3000.txt")
-  
-df.selectExpr("split(value, ' ')\
-as Text_Data_In_Rows_Using_format_load").show(4,False)
+import pandas
+pd = pandas.read_csv('3000.txt', sep=" ", header=None)
+output = pd.T
+output.to_csv(r'SmallDatSet.txt', header=None)
