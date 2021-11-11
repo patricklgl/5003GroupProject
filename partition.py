41a17741bfba1a48205e1b3c34937b74935c77f9
stevenTCL <steven351906@yahoo.com.hk>
2021-11-12 01:03:49 +0800

    add class header

--- partition.py
+++ partition.py
@@ -1,1 +1,17 @@
+# Create a class with 2 methods spilt and enlarge as mentioned 
+
+Class Partition(object):
+  def __init__(self, data):
+    self = self
+   
+#  def create_partitions(self, data, box):
+  
+  def spilt(self, data):
+    return
+    
+  
+  def enlarge(self, data):
+    return 
+  
+# Do the partition
 
