35f1e489c1c9a49690f26fcfc3d091cbe85bb018
cheungshirley <57556151+cheungshirley@users.noreply.github.com>
2021-11-11 10:02:00 +0800

    Update dbscanonspark.py

--- dbscanonspark.py
+++ dbscanonspark.py
@@ -1,1 +1,8 @@
-
+from pyspark.sql import SparkSession
+  
+spark = SparkSession.builder.getOrCreate()
+  
+df = spark.read.format("text").load("3000.txt")
+  
+df.selectExpr("split(value, ' ')\
+as Text_Data_In_Rows_Using_format_load").show(4,False)
