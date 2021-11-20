f51307da87adf3f4072d1ec24b5b1a9440c282bc
cheungshirley <57556151+cheungshirley@users.noreply.github.com>
2021-11-21 00:34:04 +0800

    Add files via upload

--- dbscanonspark.py
+++ dbscanonspark.py
@@ -1,4 +1,21 @@
 import pandas
-pd = pandas.read_csv('3000.txt', sep=" ", header=None)
-output = pd.T
-output.to_csv(r'SmallDataSet.txt', header=None, index=None)
+# import pyspark as ps
+import sklearn.cluster as skc
+import numpy as np
+from dbscan import DBScan
+
+if __name__ == '__main__':
+    pd = pandas.read_csv('3000.txt', sep=" ", header=None)
+    output = pd.T
+    output.to_csv(r'SmallDataSet.txt', header=None, index=None)
+
+    m = pandas.read_csv('SmallDataSet.txt', sep=" ", header=None)
+    m1 = np.matrix(m)
+    print (m1)
+    
+    eps = 2
+    min_points = 2
+
+    DB = DBScan()
+
+    print(DB.dbscan(m1, eps, min_points))
