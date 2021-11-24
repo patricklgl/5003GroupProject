005b175d16cdd5b4a9929d656d744daef81cf196
Shirley Cheung <57556151+cheungshirley@users.noreply.github.com>
2021-11-24 23:03:12 +0800

    Add files via upload

--- dbscanonspark.py
+++ dbscanonspark.py
@@ -1,7 +1,8 @@
 import pandas
-# import pyspark as ps
+import pyspark as ps
 import sklearn.cluster as skc
 import numpy as np
+import datetime
 from dbscan import DBScan
 
 if __name__ == '__main__':
@@ -10,16 +11,24 @@ if __name__ == '__main__':
     output.to_csv(r'SmallDataSet.txt', header=None, index=None)
 
     m = pandas.read_csv('SmallDataSet.txt', sep=",", header=None)
+    
+    rd = datetime.datetime.now()
+    print("Run DBScan - ", rd)
+    
     m1 = np.matrix(m)
     print (m1)
     
-    eps = 2300
+    eps = 200
     min_points = 2
-
     DB = DBScan()
 
     print(DB.dbscan(m1, eps, min_points))
     
     X = np.array(pd)
     K = 4
-    DB.elbow(X, K)
+    DB.elbow(X,K)
+    
+    ed = datetime.datetime.now()
+    print("End DBScan - ", ed)
+    difference = ed - rd
+    print("Total time spend - ", difference.seconds, " seconds")
